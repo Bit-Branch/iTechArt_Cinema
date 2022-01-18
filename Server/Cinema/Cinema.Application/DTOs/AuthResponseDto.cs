b97edc793912ea487f8986e2948c5fb1640e3142
Bit-Branch <dorhead@mail.ru>
2022-01-18 10:30:31 +0300

    Implemented server-side auth with JWT

--- Server/Cinema/Cinema.Application/DTOs/AuthResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cinema.Application.DTO
-{
-    public class AuthResponseDto
-    {
-        public bool IsAuthSuccessful { get; set; }
-        public string ErrorMessage { get; set; }
-        public string Token { get; set; }
-    }
-}
